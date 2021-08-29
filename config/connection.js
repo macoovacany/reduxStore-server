e129a3a9838cbab30f1161da1a20dd900bfd3bc3
macoovacany <macoovacany@hotmail.com>
2021-08-29 22:03:47 +0930

    require dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
+// const uri = 'mongodb+srv://fsf-admin:A8BTDA45QbXgucv@cluster0.qektd.mongodb.net/mernshopping?retryWrites=true&w=majority'
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+  // mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
